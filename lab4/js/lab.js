f1d8149e54099446fb89cb9736fdf431b0d750e3
SingularAlanLee <alanlee4business@gmail.com>
2021-10-13 11:46:40 -0700

    A lab about using pseudocode to plan out projects

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+//A file containing pseudo code to write a program.
+//The pseudo code outlined here is used to write a program to play rock paper scissors
+//@link
+//@url
+//@author Alan Lee and Kevin Menez
+//@ since 10/13/21
+
+//while the player wants to play
+//prompt player to enter rock, paper, or scissors
+//determine computer’s selection
+//if player wins
+	//congratulations, you won!
+//else
+	//the computer won, better luck next time!
+//do you want to play again?
+//player exits play loop:  thanks for playing!
